43b1102c848250557c29dc46556766495a8732c2
Krish Koria <krishkoria2004@gmail.com>
2024-11-29 13:56:14 +0530

    Added Student Class

--- src/main/java/com/krishkoria/springbootjdbcdemo/SpringBootJdbcDemoApplication.java
+++ src/main/java/com/krishkoria/springbootjdbcdemo/SpringBootJdbcDemoApplication.java
@@ -1,13 +1,20 @@
 package com.krishkoria.springbootjdbcdemo;
 
+import com.krishkoria.springbootjdbcdemo.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootJdbcDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootJdbcDemoApplication.class, args);
+       ApplicationContext context = SpringApplication.run(SpringBootJdbcDemoApplication.class, args);
+       Student s = context.getBean(Student.class);
+       s.setRollNo(1);
+       s.setName("Krish");
+       s.setMarks("100");
+
     }
 
 }
---
+++ src/main/java/com/krishkoria/springbootjdbcdemo/model/Student.java
@@ -0,0 +1,45 @@
+package com.krishkoria.springbootjdbcdemo.model;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class Student {
+    private int rollNo;
+    private String name;
+    private String marks;
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMarks() {
+        return marks;
+    }
+
+    public void setMarks(String marks) {
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "rollNo=" + rollNo +
+                ", name='" + name + '\'' +
+                ", marks='" + marks + '\'' +
+                '}';
+    }
+}
